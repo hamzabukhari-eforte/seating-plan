2909b19d2a0879e9a139be48c9140a431e149486
hamzabukhari-eforte <hamza@eforte.net>
2026-09-08 12:04:38 +0500

    Remove unused import of floor5 and update floors array in mockData.ts

--- lib/mockData.ts
+++ lib/mockData.ts
@@ -1,5 +1,4 @@
 import { floor4 } from './floor4';
-import { floor5 } from './floor5';
 import type { Floor } from './types';
 
-export const floors: Floor[] = [floor4(), floor5()];
+export const floors: Floor[] = [floor4()];
